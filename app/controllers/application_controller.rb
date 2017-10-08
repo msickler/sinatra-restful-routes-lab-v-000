f9f7b67baa47af173989ee6261d25e88ec60fbb1
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 14:57:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     @recipe = Recipe.create(params)
     @recipe.save
-    redirect to '/recipes/#{@recipe.id}'
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
