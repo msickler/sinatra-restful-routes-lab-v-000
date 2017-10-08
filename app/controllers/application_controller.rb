af9ba28203d1bb46088008ee2cfc323812d01365
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 14:59:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,13 +35,13 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch '/recipes/:id/edit' do 
+  patch '/recipes/:id/edit' do
       @recipe = Recipe.find_by_id(params[:id])
       @recipe.name = params[:name]
       @recipe.ingredients = params[:ingredients]
       @recipe.cook_time = params[:cook_time]
       @recipe.save
-  
+
       redirect to "/recipes/#{@recipe.id}"
   end
 
